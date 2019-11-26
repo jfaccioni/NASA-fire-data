66bf0b4778b87e4170a24bd7312d32e13e38fd42
Juliano Luiz Faccioni <julianofaccioni@gmail.com>
2019-11-26 20:39:20 -0300

    Deleted setup.py (no use after moving to conda env)

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name='NASA fire data',
-    version='0.1.0',
-    packages=find_packages(), install_requires=['pandas', 'geopy', 'geopandas', 'matplotlib', 'numpy', 'seaborn']
-)
